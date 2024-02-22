28aaf0b4faf9ef716857ce7b9d9999936674d0fa
raiota <choppy.raio@gmail.com>
2024-02-22 23:30:33 +0900

    いったんすべて管理対象から外す

--- .config/nvim/after/plugin/colorscheme.rc.lua
+++
@@ -1,9 +0,0 @@
--- # Nord
-vim.cmd [[colorscheme nord]]
-
--- ## configuration
-vim.g.nord_borders = true
-vim.g.nord_disable_background = true
-vim.g.nord_uniform_diff_background = true
-
-require('nord').set()
